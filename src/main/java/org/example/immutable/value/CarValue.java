21cb25c69dca413ca42174f6b676a5ebfd8c8a5d
Jinghai Ren <renjinghai@gmail.com>
2020-09-07 18:22:21 -0700

    Comment "the method name has to be getXXX" for immutable value.

--- src/main/java/org/example/immutable/value/CarValue.java
+++ src/main/java/org/example/immutable/value/CarValue.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Value.Immutable
 public interface CarValue {
 
+    //the method name has to be getXXX
     String getMake();
     int getNumberOfSeats();
     List<WheelValue> getWheels();
